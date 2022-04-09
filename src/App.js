f47bd962309d06fe33dbc372f4a07055dfca3cfa
Bonie <mboniface107@gmail.com>
2022-04-09 11:56:14 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     const [selectedPunk, setSelectedPunk] = useState(0);
     useEffect(() => {
         const getMyNfts = async () => {
-            const openSeadata = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x5AE17A3ff84b10c047Fe1685502Ce92911ff13b3&order_direction=asc')
+            const openSeadata = await axios.get('https://testnets-api.opensea.io/assets?format=json&asset_contract_address=0x5AE17A3ff84b10c047Fe1685502Ce92911ff13b3&order_direction=asc')
             let collections = openSeadata.data.assets
             setPunkListData(collections);
             console.log(openSeadata.data.assets)
